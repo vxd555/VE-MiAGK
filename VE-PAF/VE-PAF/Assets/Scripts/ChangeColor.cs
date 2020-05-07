3229c8c2cfba83502c107785e102b8cbe80feccb
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2020-05-07 14:27:37 +0200

    Added project to cardboard vr

---
+++ VE-PAF/VE-PAF/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void SetRed()
+	{
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	public void SetBlue()
+	{
+		GetComponent<Renderer>().material.color = Color.blue;
+	}
+	public void SetBlack()
+	{
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+}
