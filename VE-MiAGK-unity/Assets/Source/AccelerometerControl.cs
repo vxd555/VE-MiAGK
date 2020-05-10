fcf31868f2c63bf3510a0111fa7d1297d6193183
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2020-05-11 00:01:35 +0200

    Added gyroscope support.

---
+++ VE-MiAGK-unity/Assets/Source/AccelerometerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelerometerControl: MonoBehaviour
+{
+	public Text accText = null;
+
+    void Update()
+    {
+		Vector3 vector = new Vector3(
+								  Mathf.Round(Input.acceleration.x * 1000) / 10,
+								  Mathf.Round(Input.acceleration.y * 1000) / 10,
+								  Mathf.Round(Input.acceleration.z * 1000) / 10);
+
+		accText.text = $"x: {vector.x}\ny: {vector.y}\nz: {vector.z}";
+    }
+}
---
+++ VE-MiAGK-unity/Assets/Source/GyroControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+	private bool gyroEnabled;
+	private Gyroscope gyro;
+
+	private Transform cameraContainer;
+	private Quaternion rot;
+
+    void Start()
+    {
+		gyroEnabled = EnableGyro();
+		cameraContainer = transform.parent;
+		//rot = new Quaternion(1, 0, 0, 0);
+	}
+
+    private bool EnableGyro()
+	{
+		if(SystemInfo.supportsGyroscope)
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+
+			rot = new Quaternion(0, 0, 1, 0);
+
+			return true;
+		}
+		return false;
+	}
+
+	public void Update()
+	{
+		if(gyroEnabled)
+		{
+			//transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+			cameraContainer.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+			transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+			//transform.rotation = gyro.attitude * rot;
+			/*transform.rotation = Quaternion.Euler(transform.eulerAngles.x,
+													transform.eulerAngles.y, 
+													transform.eulerAngles.z - 90f);*/
+		}
+
+		//transform.rotation = rot;
+	}
+}
