1ac311e04da8ae503f9fc2adad0b2e3a9e5c5a11
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2020-06-14 16:59:40 +0200

    Added FPS counter.

---
+++ VE-MiAGK-unity/Assets/Source/FPSCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text fpsCounter = null;
+    public float timer, refresh, avgFramerate, timelapse;
+
+    void Update()
+    {
+        timelapse = Time.smoothDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timelapse;
+
+        if(timer <= 0) avgFramerate = (int)(1f / timelapse);
+
+        fpsCounter.text = $"{avgFramerate} FPS";
+    }
+}
